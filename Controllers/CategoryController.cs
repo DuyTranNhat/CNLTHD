0f8a6ec9b17145019b7f15399833584009de2190
DuyTranNhat <duytn1053@gmail.com>
2024-11-10 17:56:02 +0700

    add first method

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CNLTHD.Controllers
+{
+    public class CategoryController : Controller
+    {
+        [HttpGet("getCategories")]
+        public async Task<IActionResult> getCategories([FromQuery] int page = 1, [FromQuery] int limit = 12)
+        {
+            return Ok("result");
+        }
+    }
+}
